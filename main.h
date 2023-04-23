1ae3e31d7d8e3f80358750b7dda134374e812dbf
RKodad <rn.kodad@gmail.com>
2023-04-23 14:23:09 -0700

    prototypes

--- main.h
+++ main.h
@@ -1,13 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *format, ...);
-int _putchar(char c);
-int print_string(char *);
-int print_decimal(int value);
-long long int power(int base, int x);
-long long convertToDecimal(int Number, int base);
-int print_binary(int number);
-int print_unsigned(unsigned int number);
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct Set - A specifier - function pair
+ * @spec: Char - the format specifier
+ * @print: Special function implemented to print data of a given
+ * specifier to stdout
+ *
+ * Description: A specifier - function pair
+ */
+typedef struct Set
+{
+	char spec;
+	int (*print)(va_list arg);
+} set;
+
+int print_d(va_list list);
+int print_rot13(va_list list);
+int print_rev(va_list list);
+int print_str(va_list list);
+int print_char(va_list list);
+int _printf(const char*, ...);
 
 #endif
